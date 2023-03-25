0e01cd63e9c0fb678ff448d77446332c6e6f8bdc
Hongrok Lim <hongrr123@gmail.com>
2023-03-25 23:48:57 +0900

    Added Tests

--- test/jgol_1225_ppl_watch.cc
+++ test/jgol_1225_ppl_watch.cc
@@ -5,8 +5,30 @@
 #endif
 
 #include <iostream>
+#include <algorithm>
+#include <bitset>
+
+#define MAX_N   5000
+#define PI      3.141
 
 using namespace std;
+
+typedef struct {
+  double r2;
+  int n;
+} dist_t;
+
+inline double pow2(double x) {
+  return x*x;
+}
+
+inline double dist2_of(double a[2], double b[2]) {
+  return pow2(a[0]-b[0]) + pow2(a[1]-b[1]);
+}
+
+bool compare_dist2(dist_t a, dist_t b) {
+  return a.r2 < b.r2;
+}
 
 #ifndef __LOCAL__
 int main() {
@@ -16,21 +38,51 @@ int main() {
 TEST(MainTest, main){
   FILE *sfp;
 
-  sfp = freopen("input/swea_0000.txt", "r", stdin);
+  // sfp = freopen("input/jgol_1225.txt", "r", stdin); // 2
+  // sfp = freopen("input/jgol_1225_1.txt", "r", stdin); // 2
+  // sfp = freopen("input/jgol_1225_2.txt", "r", stdin); // 0
+  sfp = freopen("input/jgol_1225_3.txt", "r", stdin); // 4
   EXPECT_NE(sfp, nullptr);
 #endif
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int TC;
-  cin >> TC;
+  int N;
+  double K, r2;
+  double src[2][2], tgt[MAX_N+1][2];
+  dist_t dist2[2][MAX_N+1];
 
-  for (int tc=1; tc<=TC; tc++) {
-    cout << "#" << tc << " " << 1 << endl;
+  cin >> N >> src[0][0] >> src[0][1] >> src[1][0] >> src[1][1] >> K;
+  r2 = K / PI;
+
+  for (int n=1; n<=N; n++) {
+    cin >> tgt[n][0] >> tgt[n][1];
+    dist2[0][n] = {dist2_of(src[0], tgt[n]), n};
+    dist2[1][n] = {dist2_of(src[1], tgt[n]), n};
   }
+
+  sort(&dist2[0][1], &dist2[0][N+1], compare_dist2);
+  sort(&dist2[1][1], &dist2[1][N+1], compare_dist2);
+  
+  bitset<MAX_N> mark_a, mark_b, mark_ab;
+  for (int n=1; n<=N; n++)
+    mark_a[dist2[0][n].n] = 1;
+
+  int n_a = N, n_b = 0, max_cnt;
+  while (n_a>=0) {
+    if (dist2[0][n_a].r2 <= r2) {
+      while ((n_b+1) <= N && dist2[0][n_a].r2+dist2[1][n_b+1].r2 <= r2) {
+        mark_b[dist2[1][++n_b].n] = 1;
+      }
+      mark_ab = mark_a | mark_b;
+      if (max_cnt < mark_ab.count()) max_cnt = mark_ab.count();
+    }
+    mark_a[dist2[0][n_a--].n] = 0;
+  }
+
+  cout << (N - max_cnt) << endl;
 
 #ifndef __LOCAL__
   return 0;
 #endif
 }
-
--- test/jgol_3336_rec_polygon.cc
+++ test/jgol_3336_rec_polygon.cc
@@ -5,8 +5,61 @@
 #endif
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
+
+typedef struct {
+  int x;
+  int y;
+} pos_t;
+
+typedef struct {
+  int min;
+  int max;
+} line_t;
+
+line_t *xs, *ys;
+int xn = 0, yn = 0;
+vector<int> v_max;
+
+bool by_range(line_t a, line_t b) {
+  return a.min < b.min || (a.min == b.min && a.max < b.max);
+}
+
+void add_line(pos_t prev_p, pos_t p) {
+  if (prev_p.x == p.x) {
+    if (prev_p.y < p.y) {
+      xs[xn++] = {prev_p.y, p.y};
+    } else {
+      xs[xn++] = {p.y, prev_p.y};
+    }
+  } else {
+    if (prev_p.x < p.x) {
+      ys[yn++] = {prev_p.x, p.x};
+    } else {
+      ys[yn++] = {p.x, prev_p.x};
+    }
+  }
+}
+
+int max_meets(line_t *as, int an) {
+  v_max.clear();
+  int i = 0, max_v = as[an-1].max, max_cnt = 0;
+
+  for(int v=as[0].min; v<max_v; v++) {
+    while (i<an && as[i].min<=v)
+      v_max.emplace_back(as[i++].max);
+
+    for (auto it=v_max.begin(); it<v_max.end(); it++)
+      if (*it <= v) v_max.erase(it--);
+
+    if (max_cnt < v_max.size()) max_cnt = v_max.size();
+  }
+
+  return max_cnt;
+}
 
 #ifndef __LOCAL__
 int main() {
@@ -15,22 +68,54 @@ int main() {
 #ifdef __LOCAL__
 TEST(MainTest, main){
   FILE *sfp;
-
-  sfp = freopen("input/swea_0000.txt", "r", stdin);
+  sfp = freopen("input/jgol_3336.txt", "r", stdin);
   EXPECT_NE(sfp, nullptr);
 #endif
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int TC;
-  cin >> TC;
+  int N;
+  cin >> N;
 
-  for (int tc=1; tc<=TC; tc++) {
-    cout << "#" << tc << " " << 1 << endl;
+  xs = new line_t[N/2];
+  ys = new line_t[N/2];
+
+  pos_t first_p, prev_p, p;
+
+  cin >> first_p.x >> first_p.y;
+  prev_p = first_p;
+
+  for (int i=1; i<N; i++) {
+    cin >> p.x >> p.y;
+    add_line(prev_p, p);
+    prev_p = p;
   }
+  
+  p = first_p;
+  add_line(prev_p, first_p);
+
+  v_max.reserve(xn);
+
+  sort(xs, &xs[xn], by_range);
+  int max_x = max_meets(xs, xn);
+
+  sort(ys, &ys[yn], by_range);
+  int max_y = max_meets(ys, yn);
+
+#ifdef __LOCAL__
+  for (int i=0; i<xn; i++)
+    cout << xs[i].min << ", " << xs[i].max << endl;
+  cout << endl;
+  for (int i=0; i<yn; i++)
+    cout << ys[i].min << ", " << ys[i].max << endl;
+#endif
+
+  cout << ((max_x > max_y) ? max_x : max_y) << endl;
+
+  delete[] xs;
+  delete[] ys;
 
 #ifndef __LOCAL__
   return 0;
 #endif
 }
-
